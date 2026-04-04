9d348b8ad153ca457c8d6202080fcc336792eb52
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-04-04 21:25:52 +0600

    Update traffic.sh with new headers and values

--- traffic.sh
+++ traffic.sh
@@ -4,21 +4,23 @@ host: b-graph.facebook.com
 
 x-fb-request-analytics-tags: {"network_tags":{"product":"350685531728","request_category":"graphql","purpose":"fetch","retry_attempt":"0"},"application_tags":"graphservice"}
 
-x-fb-rmd: fail=Server:INVALID_MAP,Default:INVALID_MAP;v=;ip=;tkn=;reqTime=4784231;recvTime=6553710
-
 priority: u=0
 
 content-encoding: gzip
 
 x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
 
-user-agent: [FBAN/FB4A;FBAV/544.0.0.42.272;FBBV/862539700;FBDM/{density=1.7125,width=720,height=1558};FBLC/en_US;FBRV/0;FBCR/TelKila;FBMF/Google;FBBD/Google;FBPN/com.facebook.katana;FBDV/Pixel;FBSV/10;FBOP/1;FBCA/arm64-v8a:;]
+user-agent: [FBAN/FB4A;FBAV/555.0.0.49.59;FBBV/926292439;FBDM/{density=1.7125,width=720,height=1558};FBLC/en_US;FBRV/0;FBCR/TelKila;FBMF/Google;FBBD/Google;FBPN/com.facebook.katana;FBDV/Pixel;FBSV/10;FBOP/1;FBCA/arm64-v8a:;]
 
-x-fb-friendly-name: FbBloksActionRootQuery-com.bloks.www.bloks.caa.login.async.headers_process_transparency_event
+x-fb-friendly-name: FbBloksActionRootQuery-com.bloks.www.bloks.caa.login.async.send_login_request
+
+x-zero-f-device-id: 524ecc92-d82a-4972-9270-286dc45dfaaa
+
+x-fb-integrity-machine-id: kSbRaYEMa-jUTDL1vrAP-ML0
 
 x-graphql-request-purpose: fetch
 
-x-fb-device-group: 3536
+x-fb-device-group: 7359
 
 x-tigon-is-retry: False
 
@@ -36,9 +38,11 @@ x-zero-state: unknown
 
 x-meta-zca: empty_token
 
-app-scope-id-header: b37b4955-50c8-406f-b13d-865295b134f1
+app-scope-id-header: 524ecc92-d82a-4972-9270-286dc45dfaaa
 
-x-fb-connection-type: unknown
+x-fb-connection-type: WIFI
+
+x-meta-usdid: 3a4465b6-2e4b-4762-bf43-c043627015ee.1775317772.MEQCIBoUDnoVvQHUyP69xBkXmo5Vig1Xsco9dAMN04BWnC7XAiAwB1mGduOlWh0bOS7SB4H84kFWcpm2jRMy21lDajuPAw
 
 accept-encoding: gzip, deflate
 
@@ -48,4 +52,4 @@ x-fb-client-ip: True
 
 x-fb-server-cluster: True
 
-x-fb-conn-uuid-client: fa2b94cee3f809aa38d4d4ce4558ac63
+x-fb-conn-uuid-client: NC7aBfh7ogfp9SEYazIN4A==
