477e9248637cf7db151c88959693664bd2eed379
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-05-01 18:54:52 +0600

    Update headers in traffic.sh for API requests

--- traffic.sh
+++ traffic.sh
@@ -4,47 +4,45 @@ host: b-graph.facebook.com
 
 x-fb-request-analytics-tags: {"network_tags":{"product":"350685531728","request_category":"graphql","purpose":"fetch","retry_attempt":"0"},"application_tags":"graphservice"}
 
-x-fb-rmd: state=URL_ELIGIBLE
-
 priority: u=0
 
 content-encoding: gzip
 
-x-fb-device-group: 3939
+x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
+
+user-agent: [FBAN/FB4A;FBAV/558.0.0.70.72;FBBV/953309009;FBDM/{density=1.7125,width=720,height=1505};FBLC/en_US;FBRV/0;FBCR/Airtel;FBMF/Symphony;FBBD/Symphony;FBPN/com.facebook.shakib;FBDV/Z35;FBSV/11;FBOP/1;FBCA/arm64-v8a:;]
+
+x-fb-friendly-name: FbBloksActionRootQuery-com.bloks.www.bloks.caa.login.async.send_login_request
+
+x-zero-f-device-id: c312d12c-897b-4ef4-b942-9067d3e78926
 
 x-fb-integrity-machine-id: 3WfeaeR7x1o2W7LHKOJJWt2M
 
-x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
-
-user-agent: [FBAN/FB4A;FBAV/556.1.0.63.64;FBBV/937871976;FBDM/{density=1.7125,width=720,height=1505};FBLC/en_US;FBRV/0;FBCR/Airtel;FBMF/Symphony;FBBD/Symphony;FBPN/com.facebook.shakib;FBDV/Z35;FBSV/11;FBOP/1;FBCA/arm64-v8a:;]
-
 x-graphql-request-purpose: fetch
 
-x-fb-friendly-name: FbBloksActionRootQuery-com.bloks.www.bloks.caa.login.async.headers_process_transparency_event
-
-x-zero-f-device-id: 4f716d42-290a-4c5e-a77d-46ba52be80b0
+x-fb-device-group: 1993
 
 x-tigon-is-retry: False
 
-x-zero-state: unknown
-
 x-graphql-client-library: graphservice
-
-x-fb-sim-hni: 47007
 
 content-type: application/x-www-form-urlencoded
 
 x-fb-net-hni: 47002
 
+x-fb-sim-hni: 47007
+
 authorization: OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32
+
+x-zero-state: unknown
 
 x-meta-zca: empty_token
 
-app-scope-id-header: 03b27701-72cb-4699-b75e-c9123e986f51
+app-scope-id-header: a7a67d0a-5abe-4adb-a2bb-989c246fe4d6
 
 x-fb-connection-type: WIFI
 
-x-meta-usdid: 6f8619f1-f4f3-496a-984a-4115404af94a.1776240474.MEQCIDRpXCR3ssxKqrUslupI0v4-7MF98sDrNKZNS2tJD7xPAiBr0MJNYN8VUa4B9_2kGCj-2EZ9gqCH5iEKXmH-BHYUzg
+x-meta-usdid: 94ffd834-5633-4d5b-a918-6aef0bd7079a.1777638782.MEUCIEki4XJcvZWb0EPieFLtLoyKxKgVTuwDOkfJTZfjLqh1AiEAg09KLUaOsjDa79-UIwH0wBDOSy3eOyT9Vcr2m8R0BC4
 
 accept-encoding: gzip, deflate
 
@@ -54,4 +52,5 @@ x-fb-client-ip: True
 
 x-fb-server-cluster: True
 
-x-fb-conn-uuid-client: oY641/IWNocCgLitE/Dw5Q==
+x-fb-conn-uuid-client: jDNJ4oDiGLknRwdlkqvJsA==
+
