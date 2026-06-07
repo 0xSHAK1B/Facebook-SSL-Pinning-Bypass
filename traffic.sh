635d2ffac41f1b9d281cada359c0a5c033608452
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-06-07 21:35:03 +0600

    Update user-agent and various headers in traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -10,19 +10,19 @@ priority: u=0
 
 content-encoding: gzip
 
-x-fb-device-group: 7018
+x-fb-device-group: 6517
 
 x-fb-integrity-machine-id: 3WfeaeR7x1o2W7LHKOJJWt2M
 
 x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
 
-user-agent: [FBAN/FB4A;FBAV/561.0.0.42.67;FBBV/968460367;FBDM/{density=1.7125,width=720,height=1505};FBLC/en_US;FBRV/0;FBCR/Airtel;FBMF/Symphony;FBBD/Symphony;FBPN/com.facebook.shakib;FBDV/Z35;FBSV/11;FBOP/1;FBCA/arm64-v8a:;]
+user-agent: [FBAN/FB4A;FBAV/563.1.0.50.73;FBBV/982687402;FBDM/{density=1.7125,width=720,height=1505};FBLC/en_US;FBRV/0;FBCR/Airtel;FBMF/Symphony;FBBD/Symphony;FBPN/com.facebook.shakib;FBDV/Z35;FBSV/11;FBOP/1;FBCA/arm64-v8a:;]
 
 x-graphql-request-purpose: fetch
 
-x-fb-friendly-name: FbBloksActionRootQuery-com.bloks.www.caa.login.oauth.token.fetch.async
+x-fb-friendly-name: FbBloksAppRootQuery-com.bloks.www.caa.assistive_login_confirmation
 
-x-zero-f-device-id: ed06d124-dc76-4fdd-83a9-74bb8726bdf6
+x-zero-f-device-id: c56a2371-50e1-46d4-98ae-934afc417579
 
 x-tigon-is-retry: False
 
@@ -40,11 +40,11 @@ authorization: OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32
 
 x-meta-zca: empty_token
 
-app-scope-id-header: ed06d124-dc76-4fdd-83a9-74bb8726bdf6
+app-scope-id-header: b8bfed37-4890-4ad9-9535-12c1e2619758
 
 x-fb-connection-type: WIFI
 
-x-meta-usdid: d523aaf4-fdf9-4fe6-9527-ae1dd2752376.1779294734.MEQCIAi7ocMJ1dniHEIQBQWDHexYwX-NHiZ0esct59fb4FwQAiBh4AXBT0g7MBkUC7ZBoJlxax6azQqrCNEBLxCT3UxLcw
+x-meta-usdid: 6023135c-0348-4f37-bc05-0e66fc6b832b.1780849678.MEQCIGAO8rPeJxL5_TP9wm4BWc9goDAYZGBZpwKryxZt0TntAiAoOksJ4QCPSMD_mHTdmG13_Qrsxwrn966AxjCN_yqGng
 
 accept-encoding: gzip, deflate
 
@@ -54,4 +54,5 @@ x-fb-client-ip: True
 
 x-fb-server-cluster: True
 
-x-fb-conn-uuid-client: qj4lnpMNfyJEG1QjPKBjoA==
+x-fb-conn-uuid-client: +u2XE369HWzffh08E2wlzw==
+
