e09795517cd627a5964461c94ce44a89c4fa38b7
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-06-19 18:08:36 +0600

    Update HTTP headers in traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -1,58 +1,27 @@
 POST /graphql HTTP/2
-
 host: b-graph.facebook.com
-
 x-fb-request-analytics-tags: {"network_tags":{"product":"350685531728","request_category":"graphql","purpose":"fetch","retry_attempt":"0"},"application_tags":"graphservice"}
-
 x-fb-rmd: state=URL_ELIGIBLE
-
 priority: u=0
-
-content-encoding: gzip
-
-x-fb-device-group: 6517
-
-x-fb-integrity-machine-id: 3WfeaeR7x1o2W7LHKOJJWt2M
-
-x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
-
-user-agent: [FBAN/FB4A;FBAV/563.1.0.50.73;FBBV/982687402;FBDM/{density=1.7125,width=720,height=1505};FBLC/en_US;FBRV/0;FBCR/Airtel;FBMF/Symphony;FBBD/Symphony;FBPN/com.facebook.shakib;FBDV/Z35;FBSV/11;FBOP/1;FBCA/arm64-v8a:;]
-
+user-agent: [FBAN/FB4A;FBAV/566.0.0.48.73;FBBV/997672765;FBDM/{density=2.625,width=1080,height=2392};FBLC/en_GB;FBRV/0;FBCR/;FBMF/Nothing;FBBD/Nothing;FBPN/com.facebook.katana;FBDV/A001;FBSV/15;FBOP/1;FBCA/arm64-v8a:;]
+x-fb-friendly-name: FbBloksActionRootQuery-com.bloks.www.bloks.caa.login.async.send_login_request
+x-zero-f-device-id: c6c44de3-5dab-416f-98f4-25b534b14f83
+x-fb-integrity-machine-id: HbwzapHp3OyT2xPoCYEGQoFT
 x-graphql-request-purpose: fetch
-
-x-fb-friendly-name: FbBloksAppRootQuery-com.bloks.www.caa.assistive_login_confirmation
-
-x-zero-f-device-id: c56a2371-50e1-46d4-98ae-934afc417579
-
+x-fb-device-group: 5952
 x-tigon-is-retry: False
-
-x-zero-state: unknown
-
 x-graphql-client-library: graphservice
-
-x-fb-sim-hni: 47007
-
 content-type: application/x-www-form-urlencoded
-
-x-fb-net-hni: 47002
-
+x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
+content-encoding: gzip
 authorization: OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32
-
+x-zero-state: unknown
 x-meta-zca: empty_token
-
-app-scope-id-header: b8bfed37-4890-4ad9-9535-12c1e2619758
-
+app-scope-id-header: ec69afe5-8bec-4d5b-bf22-43120c7ff64e
 x-fb-connection-type: WIFI
-
-x-meta-usdid: 6023135c-0348-4f37-bc05-0e66fc6b832b.1780849678.MEQCIGAO8rPeJxL5_TP9wm4BWc9goDAYZGBZpwKryxZt0TntAiAoOksJ4QCPSMD_mHTdmG13_Qrsxwrn966AxjCN_yqGng
-
+x-meta-usdid: 6b6c4be5-6acd-46c8-8692-2b278e65384f.1781872468.MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEemsarJJ7WsIxr5rOxDBUjrwxHRRzIyVB9RWRIa8ph406xsy-AO9cBIXRujoEVzO4WlIOAPqnDv6THyyYT4DY9g.MEYCIQDKfAggdbQiZb2T7nWEaAo234Ns9hFt9djDB_OePvoyxgIhAL4_lFWWvNdM8AiQLZhT4Slur_WiUrBwfYnebUVtIS64
 accept-encoding: gzip, deflate
-
 x-fb-http-engine: Tigon/Liger
-
 x-fb-client-ip: True
-
 x-fb-server-cluster: True
-
-x-fb-conn-uuid-client: +u2XE369HWzffh08E2wlzw==
-
+x-fb-conn-uuid-client: BC7XZoFhomuWJDOhx78DSw==
