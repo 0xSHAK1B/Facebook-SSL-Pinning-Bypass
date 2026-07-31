78c27f5d0b0ab676b3b4ca52323d6220e11f2348
MUHAMMAD SHAKIB <shakibb1928u@gmail.com>
2026-07-31 21:07:39 +0600

    Update traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -1,29 +1,28 @@
-POST /graphql HTTP/2
-host: b-graph.facebook.com
-x-fb-request-analytics-tags: {"network_tags":{"product":"350685531728","request_category":"graphql","purpose":"fetch","retry_attempt":"0"},"application_tags":"graphservice"}
-x-fb-rmd: state=URL_ELIGIBLE
-priority: u=0
-content-encoding: gzip
-x-fb-device-group: 5156
-x-fb-integrity-machine-id: baQ7av2MiLltRYMvL8e5KN1B
-x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
-user-agent: [FBAN/FB4A;FBAV/570.0.0.24.72;FBBV/1017250544;FBDM/{density=2.625,width=1080,height=2392};FBLC/en_GB;FBRV/0;FBCR/Robi;FBMF/Nothing;FBBD/Nothing;FBPN/com.facebook.katana;FBDV/A001;FBSV/15;FBOP/1;FBCA/arm64-v8a:;]
-x-graphql-request-purpose: fetch
-x-fb-friendly-name: FbBloksAsyncComponentsQuery-com.bloks.www.ccs.conversational_support.fb_forgot_password_button.component.query
-x-zero-f-device-id: da954aa8-4669-4d33-ad55-f73894e245e8
-x-tigon-is-retry: False
-x-zero-state: unknown
-x-graphql-client-library: graphservice
-x-fb-sim-hni: 47002
-content-type: application/x-www-form-urlencoded
-x-fb-net-hni: 47002
-authorization: OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32
-x-meta-zca: empty_token
-app-scope-id-header: b76c69e4-be6e-4112-8b77-721e1bc810f8
-x-fb-connection-type: WIFI
-x-meta-usdid: bde7862d-9dc6-4334-be33-49795001959e.1784570274.MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEoUsv4OhEHB3_OAS4tiVwykjUbM_XStGu0JZxL5ReUZswxIOdr18gS-0et2edLpq3__gtz2TRvKPQ-naE4xiSDg.MEQCIHgopp-5jPkLcGQKWEDMnWiRRlgUrJ2oertsldGQ95K9AiANQHtsEgeUXWIbMNmn7pFHq1p0aVHNQQX-2Dnol7mZog
-accept-encoding: gzip, deflate
-x-fb-http-engine: Tigon/Liger
-x-fb-client-ip: True
-x-fb-server-cluster: True
-x-fb-conn-uuid-client: Kxa/+k2Q5CNRFdFAQnB9gg==
+curl --http2 -L -X POST 'https://b-graph.facebook.com/graphql' \
+-H 'User-Agent: [FBAN/FB4A;FBAV/572.0.0.38.71;FBBV/1027405359;FBDM/{density=2.625,width=1080,height=2392};FBLC/en_GB;FBRV/0;FBCR/Robi;FBMF/Nothing;FBBD/Nothing;FBPN/com.facebook.katana;FBDV/A001;FBSV/15;FBOP/1;FBCA/arm64-v8a:;]' \
+-H 'Accept-Encoding: gzip, deflate' \
+-H 'Content-Type: application/x-www-form-urlencoded' \
+-H 'x-fb-request-analytics-tags: {"network_tags":{"product":"350685531728","request_category":"graphql","purpose":"fetch","retry_attempt":"0"},"application_tags":"graphservice"}' \
+-H 'x-fb-rmd: state=URL_ELIGIBLE' \
+-H 'priority: u=0' \
+-H 'content-encoding: gzip' \
+-H 'x-fb-device-group: 5156' \
+-H 'x-fb-integrity-machine-id: baQ7av2MiLltRYMvL8e5KN1B' \
+-H 'x-zero-eh: 664c0faaac849cb891d0a261fbb72a12' \
+-H 'x-graphql-request-purpose: fetch' \
+-H 'x-fb-friendly-name: FbBloksActionRootQuery-com.bloks.www.bloks.caa.login.async.headers_process_transparency_event' \
+-H 'x-zero-f-device-id: da954aa8-4669-4d33-ad55-f73894e245e8' \
+-H 'x-tigon-is-retry: False' \
+-H 'x-zero-state: unknown' \
+-H 'x-graphql-client-library: graphservice' \
+-H 'x-fb-sim-hni: 47002' \
+-H 'x-fb-net-hni: 47002' \
+-H 'authorization: OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32' \
+-H 'x-meta-zca: empty_token' \
+-H 'app-scope-id-header: bfd3d392-96ea-4e9a-8436-bcc9e0f1a676' \
+-H 'x-fb-connection-type: WIFI' \
+-H 'x-meta-usdid: c842c9d7-356d-4101-9266-c0ca1e5020bc.1785512975.MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEUD65yIhodYm-M6AxpZpPL53PSUBcPgaPICkIBEtQo4ZE1f-8LXhzuQ5sDKlNgUTDoDXsviA39LEFz9ZdrhJokQ.MEYCIQC_oaXqEKPycSOt2b5ucAJNqeMlAfoxghqVgKbEAJaXRQIhAOlBC3LcFlVeSDPIyWcwArlBHNWIytNndUR50laF6Lo5' \
+-H 'x-fb-http-engine: Tigon/Liger' \
+-H 'x-fb-client-ip: True' \
+-H 'x-fb-server-cluster: True' \
+-H 'x-fb-conn-uuid-client: NssqsnrEQUChuJxJqYHHzA==' \
